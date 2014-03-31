5a8d2a9af5d1bf357edf82410e06ff7df721f70e
analog-nico <nicolai.kamenzky@testrails.org>
2014-03-31 21:09:00 +0200

    grunt ci now test with and without coverage analysis

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,9 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     run: {
-      // 'jasmine-node' needs to run in a separate task because the coverage report is created when grunt exits.
-      // When we use watch grunt keeps running and no report would be generated if 'jasmine-node' would be executed directly.
-      jasmine_node: {
+      jasmine_node_with_coverage: {
         cmd: 'node_modules/.bin/grunt',
-        args: ['jasmine_node']
+        args: ['jasmine_node_with_coverage']
       }
     },
     clean: {
@@ -24,10 +22,18 @@ module.exports = function (grunt) {
         jshintrc: '.jshintrc'
       }
     },
-    jasmine_node: {
+    jasmine_node_with_coverage: {
       coverage: {
         excludes: ['test/**', 'coverage/**', 'Gruntfile.js']
       },
+      options: {
+        matchall: true,
+        specFolders: ['test/'],
+        isVerbose: true
+      }
+    },
+    jasmine_node_no_coverage: {
+      coverage: false,
       options: {
         matchall: true,
         specFolders: ['test/'],
@@ -50,7 +56,24 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jasmine-node-coverage');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('test', ['clean', 'jshint', 'run:jasmine_node', 'watch']);
-  grunt.registerTask('ci', ['clean', 'jshint', 'jasmine_node']);
+
+  grunt.registerTask('jasmine_node_with_coverage', 'Unit testing with coverage report', function () {
+    grunt.config.set('jasmine_node', grunt.config.get('jasmine_node_with_coverage'));
+    grunt.task.run('jasmine_node');
+  });
+
+  grunt.registerTask('jasmine_node_no_coverage', 'Just unit testing', function () {
+    grunt.config.set('jasmine_node', grunt.config.get('jasmine_node_no_coverage'));
+    grunt.task.run('jasmine_node');
+  });
+
+  // 'jasmine-node' needs to run in a separate task because the coverage report is created when grunt exits.
+  // When we use watch grunt keeps running and no report would be generated if 'jasmine-node' would be executed directly.
+  grunt.registerTask('test', ['clean', 'jshint', 'run:jasmine_node_with_coverage', 'watch']);
+
+  // 'jasmine-node' needs to run in a separate task the second time it is executed so that es-sequence freshly required.
+  // The unit tests assume that es-sequence is not initialized in the beginning.
+  grunt.registerTask('ci', ['clean', 'jshint', 'jasmine_node_no_coverage', 'run:jasmine_node_with_coverage']);
+
   grunt.registerTask('default', ['test']);
 };
